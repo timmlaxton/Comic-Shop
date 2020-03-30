6ff117ce00d80a48fe52b5ff0878fa6b328d82a3
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-03-30 17:23:03 +0100

    tokens added to authentication

--- server/server.js
+++ server/server.js
@@ -19,6 +19,10 @@ const {User} = require('./models/user')
 
 //User//
 
+app.get('/api/users/auth',(req,res)=>{
+    
+})
+
 app.post('/api/user/register',(req,res)=>{
     const user = new User(req.body);
 
@@ -28,6 +32,26 @@ app.post('/api/user/register',(req,res)=>{
             success: true,
             userdata:doc
         })
+    })
+});
+
+app.post('/api/users/login',(req,res)=> {
+    
+    User.findOne({'email':req.body.email}, (err, user)=>{
+        if(!user) return res.json({loginSuccess:false,message: "Auth failed, email not found"});
+
+        user.comparePassword(req.body.password,(err, isMatch)=>{
+            if(!isMatch) return res.json({loginSuccess:false,message:'Wrong password'});
+
+            user.generateToken((err,user)=> {
+                if(err) return res.status(400).send(err);
+                res.cookie('w_auth', user.token).status(200).json({
+                    loginSuccess: true
+                })
+
+            })
+        })
+
     })
 })
 
