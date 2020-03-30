ffa5db97d5308dddbd3ac98b375658216ca16328
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-03-30 16:35:09 +0100

    can add user to database

--- server/server.js
+++ server/server.js
@@ -14,6 +14,23 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+//Models//
+const {User} = require('./models/user')
+
+//User//
+
+app.post('/api/user/register',(req,res)=>{
+    const user = new User(req.body);
+
+    user.save((err,doc)=>{
+        if(err) return res.json({success:false,err})
+        res.status(200).json({
+            success: true,
+            userdata:doc
+        })
+    })
+})
+
 const port = process.env.PORT || 3002;
 app.listen(port, ()=> {
     console.log(`Server running at ${port}`)
