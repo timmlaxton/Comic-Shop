0ecbd285e6da085a86e0453163a9acf0e0de7df8
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-03-30 16:49:00 +0100

    encrypted passwords

--- server/models/user.js
+++ server/models/user.js
@@ -44,15 +44,23 @@ const userSchema = mongoose.Schema({
 userSchema.pre('save', function(next){
     var user = this;
 
-    bcrypt.genSalt(SALT_I, function(err,salt){
+
+if(user.isModified('password')){
+     bcrypt.genSalt(SALT_I, function(err,salt){
         if(err) return next(err);
 
         bcrypt.hash(user.password,salt,function(err,hash){
             if(err) return next(err);
             user.password = hash;
             next();
-        })
+        });
     })
+    
+} else{
+    next()
+}
+
+   
 })
 
 
