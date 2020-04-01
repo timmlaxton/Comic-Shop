a2e45c2efeeb3eb5934b4867d310ea1342dbedb1
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-04-01 11:25:15 +0100

    adding register

--- client/src/Components/Register_login/register.js
+++ client/src/Components/Register_login/register.js
@@ -1,25 +1,13 @@
 import React, { Component } from 'react';
-import FormField from '../utils/Form/formfield';
-import {update, generateData, isFormValid} from '../utils/Form/formActions';
-
-import {connect} from 'react-redux';
-import {loginUser} from '../../actions/user_actions';
 
 class Register extends Component {
-
-    state = {
-        
-    }
-
-
-
     render() {
         return (
             <div>
-                register
+                
             </div>
         );
     }
 }
 
-export default connect()(Register);
+export default Register;
