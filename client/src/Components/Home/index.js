7c1c6d973ec4234992a9eab8a84938011ea0baec
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-04-04 13:54:33 +0100

    homecards

--- client/src/Components/Home/index.js
+++ client/src/Components/Home/index.js
@@ -2,20 +2,31 @@ import React, { Component } from 'react';
 import HomeSlider from './home_slider';
 import HomePromotion from './home_promotions'
 
+import {connect} from 'react-redux';
+import { getProductsBySell, getProductsByArrival} from '../../actions/products_actions'
+
 
 class Home extends Component {
+
+    componentDidMount(){
+        this.props.dispatch(getProductsBySell());
+        this.props.dispatch(getProductsByArrival());
+    }
+
     render() {
         return (
             <div>
                 <HomeSlider/>
                 <HomePromotion/>
-
-                
-                 
-               
             </div>
         );
     }
 }
 
-export default Home;
+const mapStateToProps = (state) => {
+    return {
+        products: state.prop
+    }
+}
+
+export default connect(mapStateToProps)(Home);
---
+++ client/src/actions/products_actions.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import {
+    
+        GET_PRODUCTS_BY_SELL,
+        GET_PRODUCTS_BY_ARRIVAL
+       
+    } from './types';
+
+    import { PRODUCT_SERVER}  from '../components/utils/misc';
+
+export function getProductsBySell(){
+   
+    const request = axios.get(`${PRODUCT_SERVER}/articles?sortBy=sold&order=desc&limit=4`)
+            .then(response => response.data)
+
+            return {
+                type: GET_PRODUCTS_BY_SELL,
+                payload: request
+            }
+}
+
+export function getProductsByArrival(){
+
+    const request = axios.get(`${PRODUCT_SERVER}/articles?sortBy=created&order=desc&limit=4`)
+            .then(response => response.data)
+
+            return {
+                type: GET_PRODUCTS_BY_ARRIVAL,
+                payload: request
+            }
+}
