5a67d93c96222a63d7b219871b8b45a6793be166
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-04-19 15:15:51 +0100

    added user history

--- client/src/Components/User/index.js
+++ client/src/Components/User/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import UserLayout from '../../hoc/user';
 import MyButton from '../utils/button'
+import UserHistoryBlock from '../utils/User/history_block'
 
 const UserDashboard = ({user}) => {
     return (
@@ -20,13 +21,23 @@ const UserDashboard = ({user}) => {
             />
         </div>
 
-        <div className="user_nfo_panel">
+        {
+
+            user.userData.history ?
+            <div className="user_nfo_panel">
             <h1>Purchase History</h1>
             <div className="user_product_block_wrapper">
-
+            <UserHistoryBlock
+            products={user.userData.history}
+            />
             </div>
 
         </div>
+            :null
+        }
+
+
+       
 
     </div>
 
---
+++ client/src/Components/utils/User/history_block.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import moment from 'moment'
+
+const UserHistoryBlock = (props) => {
+
+
+    const renderBlocks = () => (
+        props.products ?
+        props.products.map((product,i)=>(
+            <tr key={i}>
+            <td>{moment(product.dateOfPurchase).format("MM-DD-YYYY")}</td>
+            <td> {product.name}</td>
+            <td>{product.price}</td>
+            <td>{product.quantity}</td>
+
+            </tr>
+        ))
+
+        :null
+    )
+
+
+    return (
+        <div className="history_blocks">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Date of Purchase</th>
+                        <th>Product</th>
+                        <th>Price paid</th>
+                        <th>Quantity</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {renderBlocks()}
+                </tbody>
+            </table>
+            
+        </div>
+    );
+};
+
+export default UserHistoryBlock;
