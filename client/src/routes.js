50a9537d06f59289f7ae8b2eae226b7312c75314
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-05-12 21:00:23 +0100

    route changed

--- client/src/routes.js
+++ client/src/routes.js
@@ -7,7 +7,7 @@ import Layout from './hoc/layout'
 import Comics from './components/Shop/comics'
 import Shirts from './components/Shop/shirts'
 import Home from './components/register_login';
-import RegisterLogin from './components/Register_login';
+import RegisterLogin from './components/register_login';
 import Register from './components/Register_login/register';
 import ProductPage from './components/Product';
 
