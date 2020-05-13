4485699f96aa64218bad8eeb600fbc5779dfcbc9
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-05-13 11:42:26 +0100

    reverted indexs back to index.js

--- client/src/routes.js
+++ client/src/routes.js
@@ -4,9 +4,9 @@ import {Switch, Route} from 'react-router-dom';
 import Auth from './hoc/auth';
 import Layout from './hoc/layout'
 
-import Comics from './components/Shop/comics/comics';
-import Home from './components/Home/home';
-import RegisterLogin from './components/Register_login/register_login';
+import Comics from './components/Shop/comics';
+import Home from './components/Home';
+import RegisterLogin from './components/Register_login';
 import Register from './components/Register_login/register';
 import ProductPage from './components/Product/product';
 
